a5166440ac920557a35a3f0003aae339bf1cec74
Yang <yanghansen02@gmail.com>
2026-07-08 08:49:54 +0700

    Update db collections

--- models/Users.js
+++ models/Users.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, "Please add a name"],
+        required: [true, "Please insert a name"],
     },
     email: {
         type: String,
-        required: [true, "Please add an email"],
+        required: [true, "Please insert an email"],
         unique: true,
         lowercase: true
     },
     password: {
         type: String,
-        required: [true, "Please add a password"],
+        required: [true, "Please insert a password"],
         minlength: [8, "Password must be at least 8 characters"],
     },
     role: {
