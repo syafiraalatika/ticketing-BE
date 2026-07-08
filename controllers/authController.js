c1fd96b4ad7c8fcf89e721a422d70f542ad8cba3
Yang <yanghansen02@gmail.com>
2026-07-08 10:58:20 +0700

    Added rateLimit.js and implemented to authRoutes.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/Users");
 
-
 // Generate JWT Token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
-const {
-    register, logout, login, getMe
-} = require("../controllers/authController");
-
+const { register, logout, login, getMe } = require("../controllers/authController");
 const { protect } = require("../middleware/auth");
+const limiter = require("../middleware/rateLimit");
+
+// Apply the rate limiting middleware to all requests in this router
+router.use(limiter);
 
 // Register
 router.post("/register", register);
